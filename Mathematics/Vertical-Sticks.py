9349978e9143df40c85fa2f2d5f00fa2c95f0e7f
mamtadhaker <mamta.dhaker28@gmail.com>
2019-02-03 03:01:30 +0530

    mathematics

---
+++ Mathematics/Vertical-Sticks.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import os
+import sys
+
+# Complete the solve function below.
+def solve(y,n1):
+    y=sorted(y)
+    v = 0
+    prev = 0
+    for i, yi in enumerate(y):
+        if yi != prev:
+            prev = yi
+            vi = n1 / (n1 - i)
+        v += vi
+    return("{:.2f}".format(v))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        y_count = int(input())+1
+
+        y = list(map(int, input().rstrip().split()))
+
+        result = solve(y,y_count)
+
+        fptr.write(''.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
