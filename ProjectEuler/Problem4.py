32227cdea6f91792b8f9d1c7b1d44d16e9989181
anildhaker <anildhaker777@gmail.com>
2019-02-23 02:56:27 +0530

    first commit

---
+++ ProjectEuler/Problem4.py
@@ -0,0 +1,22 @@
+import sys
+# Although this solution is not optimum but it works for a couple of test cases
+
+def isPalindrome(x):
+    return (str(x) == str(x)[::-1])
+
+def maxPal(n):
+    num = []
+    for i in range(999,99,-1):
+        for j in range(999,99,-1):
+            if (isPalindrome(i*j) and (i*j < n)):
+                num.append(i*j)
+    return max(num)
+            
+        
+    
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(maxPal(n))
+    
