887a50de0686804b288f9f548ce12c096bab1aa5
anildhaker <anildhaker777@gmail.com>
2019-02-23 02:24:23 +0530

    first commit

---
+++ ProjectEuler/LargestPrimeFact.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import sys
+import math
+
+def prime_factors(n):
+    l = []
+    while (n%2 == 0):
+        l.append(2)
+        n = n/2
+    
+    for i in range(3,int(math.sqrt(n))+1,2):
+        while (n%i == 0):
+            l.append(i)
+            n = n/i
+    if n>2:
+        l.append(n)
+    return (int(max(l)))
+
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(prime_factors(n))
+    
